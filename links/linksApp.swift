8dc6f1bcf746b62a286d5918e5310a0b98b6c197
nawsheen <nawsheen.azad06@gmail.com>
2024-07-30 13:05:35 +0100

    Initial Commit

---
+++ links/linksApp.swift
@@ -0,0 +1,17 @@
+//
+//  linksApp.swift
+//  links
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct linksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
